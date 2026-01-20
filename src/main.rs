14149c3898d344bc0ec04c6bf3edf8dde308aa3b
lizelit <lizelit.you@gmail.com>
2026-01-20 18:55:33 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow;
+use crossterm::event;
+use ratatui;
+use tui_textarea;
+
+fn main() -> std::io::Result<()> {
+    ratatui::run(|mut terminal| {
+        loop {
+            terminal.draw(|frame| frame.render_widget("Hello World!", frame.area()))?;
+            if event::read()?.is_key_press() {
+                break Ok(());
+            }
+        }
+    })
 }
