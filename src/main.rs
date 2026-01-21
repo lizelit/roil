04d010431fc7db0256b5fbc50c997bfce2e39ff1
lizelit <lizelit.you@gmail.com>
2026-01-22 01:34:42 +0900

    display files and directories

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,98 @@
-use anyhow;
-use crossterm::event;
-use ratatui;
-use tui_textarea;
+use std::{
+    fs::{self, ReadDir},
+    io,
+};
 
-fn main() -> std::io::Result<()> {
-    ratatui::run(|mut terminal| {
-        loop {
-            terminal.draw(|frame| frame.render_widget("Hello World!", frame.area()))?;
-            if event::read()?.is_key_press() {
-                break Ok(());
-            }
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+};
+
+#[derive(Debug, Default)]
+pub struct App {
+    files: Vec<String>,
+    counter: u8,
+    exit: bool,
+}
+
+impl App {
+    pub fn new() -> Self {
+        let files = fs::read_dir(".")
+            .unwrap()
+            .filter_map(|entry| {
+                entry
+                    .ok()
+                    .map(|e| e.file_name().to_string_lossy().into_owned())
+            })
+            .collect();
+
+        Self {
+            files,
+            counter: 0,
+            exit: false,
         }
-    })
+    }
+
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" Roil - File List ".bold());
+        let instructions = Line::from(" Quit <Q>");
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+        let file_list_text = self.files.join("\n");
+
+        Paragraph::new(file_list_text)
+            .block(block)
+            .render(area, buf);
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let mut app = App::new();
+    let app_result = app.run(&mut terminal);
+    ratatui::restore();
+    app_result
 }
